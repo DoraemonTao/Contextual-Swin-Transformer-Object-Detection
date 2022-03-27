37c2255bdc3313723ef2a364bf038b34b3d2c15b
DoraemonTao <mrzengt@outlook.com>
2022-03-27 10:50:45 +0800

    增加了自己的训练集和描述

---
+++ mmdet/models/backbones/self_backbones.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+from ..builder import BACKBONES
+
+@BACKBONE.register_moudle()
+class MobileNet(nn.Moudule):
+
+    def __int__(self):
+        pass
+
+    def forward(self,x):
+        pass
