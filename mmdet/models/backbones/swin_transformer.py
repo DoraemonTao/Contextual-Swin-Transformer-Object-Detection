22c2005bf53fce57ece7bb821b74ba257432ef4e
DoraemonTao <mrzengt@outlook.com>
2022-04-19 10:34:42 +0800

    修改了swin_transformer文件

--- mmdet/models/backbones/swin_transformer.py
+++ mmdet/models/backbones/swin_transformer.py
@@ -2,7 +2,7 @@
 # Swin Transformer
 # Copyright (c) 2021 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
-# Written by Tao
+# Written by Tao Zeng
 # --------------------------------------------------------
 
 import torch
